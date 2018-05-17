f8ce5196aae195ded3e2968e110986700e6075a0
fs_nanamedou <34233189+fstabin@users.noreply.github.com>
2018-05-17 20:04:27 +0900

    リバーシ基礎途中

---
+++ main.py
@@ -0,0 +1,4 @@
+from reversi_status import TeamType
+from reversi_status import ReversiStatus
+
+status = ReversiStatus()
--- reversi_status.py
+++ reversi_status.py
@@ -2,21 +2,28 @@ from enum import Enum
 class TeamType(Enum):
     BLACK = 1
     WHITE = -1
-    NONE = 0
+
+    def __neg__(self):
+        if self == TeamType.BLACK:
+            return TeamType.WHITE
+        if self == TeamType.WHITE:
+            return TeamType.BLACK
 
 class ReversiStatus:
     """リバーシゲーム本体の状態すべてをコントロールするクラス"""
-    __stoneRowLen = 8
-    __stoneRowAmount = 8
-    __stoneBuffer = list(__stoneRowLen * __stoneRowAmount) #リバーシ台
-    __currentTeamType = TeamType.BLACK
+    __mStoneRowLen = 8
+    __mStoneRowAmount = 8
+    __mStoneBuffer = list(__mStoneRowLen * __mStoneRowAmount) #リバーシ台
+    __mCurrentTeamType = TeamType.BLACK
+
+    __mStoneBufferChangeLog = list()#[x, y, list[8]]
 
     def __init__(self):
         for i in range(0, len(list)):
-            self.__stoneBuffer[i] = TeamType.NONE
+            self.__mStoneBuffer[i] = TeamType.NONE
         return
 
-    def __getStoneRef(self, x, y):
+    def __getStoneIndex(self, x, y):
         """
         x,y座標から、リバーシ台上の石の位置を特定する\n
         戻り値:
@@ -27,9 +34,32 @@ class ReversiStatus:
         例外:
             IndexError: x,yが台の範囲外の時\n
         """
-        if((x <= -1) | (self.__stoneRowLen <= x) | (y <= -1) | (self.__stoneRowLen <= y)):
-            return IndexError
-        return ref(self.__stoneBuffer[x + y * self.__stoneRowLen])
+        if((x <= -1) | (self.__mStoneRowLen <= x) | (y <= -1) | (self.__mStoneRowLen <= y)):
+            return None
+        return x + y * self.__mStoneRowLen
+
+    def checkPutable(self, x, y, aTeamType):
+        if self.getStone(x,y) != TeamType.NONE:
+            return False
+        ofsX = [-1,0,1,-1,1,-1,0,1]
+        ofsY = [-1,-1,-1,0,0,1,1,1]
+        for i in range(0,8):
+            bx = x
+            by = y
+            l = 0
+            while True:
+                bx = bx + ofsX[i]
+                by = by + ofsY[i]
+                stone = self.getStone(x,y)
+                if stone == None:
+                    return
+                elif stone == -aTeamType:
+                    l += 1
+                    break 
+                elif stone == aTeamType:
+                    if l >= 1:
+                        return True
+        return False
 
     def setStone(self, x, y, aTeamType):
         """
@@ -42,8 +72,7 @@ class ReversiStatus:
         例外:\n
             IndexError: x,yが台の範囲外の時\n
         """
-        ref = self.__getStoneRef(x, y)
-        ref = aTeamType
+        self.__mStoneBuffer[self.__getStoneIndex(x,y)]  = aTeamType
  
     def getStone(self, x, y):
         """
@@ -55,21 +84,51 @@ class ReversiStatus:
         例外:\n
             IndexError: x,yが台の範囲外の時\n
         """
-        return self.__getStoneRef(x, y)
+        return  self.__mStoneBuffer[self.__getStoneIndex(x,y)]
 
-    def putStone(delf, x, y):
+    def putStone(self, x, y):
         """
         リバーシ台上の石を強制的に変更する\n
         戻り値:\n
-            bool\n
+            変更した長さint[8]\n
         入力:\n
             x,y: int型 座標\n
             aTeamType: TeamType型 変更する後のチーム(石をなくすにはTeamType.NONE)を指定する\n
         例外:\n
             IndexError: x,yが台の範囲外の時\n
         """
-        (self.__getStoneRef(x, y)) = self.__currentTeamType
+        #すでに石が置かれているときキャンセル
+        if self.getStone(x,y) != None:
+            return None
 
-        return True
+        #石を何個反転させるか判定し
+        #石を強制的に変更する
+        noChange = True
+        ofsX = [-1,0,1,-1,1,-1,0,1]
+        ofsY = [-1,-1,-1,0,0,1,1,1]
+        revl = [0,0,0,0,0,0,0,0]#反転させる石の数
+        for i in range(0,8):
+            bx = x
+            by = y
+            while True:
+                bx = bx + ofsX[i]
+                by = by + ofsY[i]
+                stone = self.getStone(bx, by)
+                if stone == -self.__mCurrentTeamType:
+                    revl[i] += 1
+                elif stone  == self.__mCurrentTeamType:
+                    if(revl[i] >= 1):
+                        for rev in range(1, revl[i] + 1):
+                            self.setStone(x + ofsX[i] * rev,y + ofsY[i] * rev, self.__mCurrentTeamType)
+                        noChange = False
+                    break
+                else:
+                    revl[i] = 0
+                    break
+        if noChange:
+            return None
+        self.setStone(x, y, self.__mCurrentTeamType)
+        self.__mStoneBufferChangeLog.append(self.__mStoneBuffer)
+        return revl
 
 
